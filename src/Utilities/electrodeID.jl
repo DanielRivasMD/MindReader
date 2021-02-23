0fe637291920ae8a76474f8bc837ec2fc2bac6b1
DanielRivasMD <danielrivasmd@gmail.com>
2021-02-23 19:48:33 +0100

    add electrode id

---
+++ src/Utilities/electrodeID.jl
@@ -0,0 +1,54 @@
+################################################################################
+
+# Parasagittal/supra-sylvian electrodes
+ParasagittalL = [
+"Fp1",
+"F3",
+"C3",
+"P3",
+"O1",
+]
+
+ParasagittalR = [
+"Fp2",
+"F4",
+"C4",
+"P4",
+"O2",
+]
+
+# Lateral/temporal electrodes
+TemporalL = [
+"F7",
+"T3", # "T7",
+"T5", # "P7",
+]
+
+TemporalR = [
+"F8",
+"T4", # "T8",
+"T6", # "P8",
+]
+
+# Midline electrodes
+Midline = [
+"Fz",
+"Cz",
+"Pz",
+]
+
+# Earlobe electrodes
+Earlobe = [
+"A1",
+"A2",
+]
+
+elecID = [ParasagittalL;
+ParasagittalR;
+TemporalL;
+TemporalR;
+Midline;
+Earlobe;
+]
+
+################################################################################
