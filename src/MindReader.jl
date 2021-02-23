dc8f06c722c0d568856ece9be99a63549fc9694d
DanielRivasMD <danielrivasmd@gmail.com>
2021-02-23 19:38:36 +0100

    add file reader edf

--- src/MindReader.jl
+++ src/MindReader.jl
@@ -9,6 +9,14 @@ include( "Utilities/argParser.jl" );
 
 ################################################################################
 
+
+#  declare tool directories
+utilDir    = "Utilities/"
+#  load functions
+@info("Loading modules...")
+include( string(utilDir,    "fileReaderEDF.jl") );
+
+################################################################################
 end
 
 ################################################################################
