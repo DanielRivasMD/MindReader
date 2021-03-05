fe457b2e778ef986c1a8f4f47285f51382d60b85
DanielRivasMD <danielrivasmd@gmail.com>
2021-03-05 01:11:55 +0100

    add autoencoder

---
+++ src/Architect/autoencoder.jl
@@ -0,0 +1,50 @@
+################################################################################
+
+import Flux: mse, throttle, ADAM
+import Parameters: @with_kw
+import CUDAapi
+
+################################################################################
+
+if CUDAapi.has_cuda()
+  # @info "CUDA is on"
+  import CuArrays
+  CuArrays.allowscalar(false)
+end
+
+################################################################################
+
+"""
+
+    modelTrain(inputAr, model, Params;
+    kws...)
+
+Train autoencoder
+
+# Arguments
+`inputAr` array to train on
+
+`model` neural network architecture
+
+arguments passed as `Params` with `Parameters::@with_kw`
+
+"""
+function modelTrain(inputAr, model, Params)
+  args = Params()
+
+  @info("Loading data...")
+  trainAr = args.device.(inputAr)
+
+  @info("Training model...")
+  loss(x) = Flux.mse(model(x), x)
+
+  # training
+  evalcb = Flux.throttle(() -> @show(Flux.loss(trainAr[1])), args.throttle)
+  opt = Flux.ADAM(args.η)
+
+  # @epochs args.epochs Flux.train!(loss, params(model), zip(trainAr), opt, cb = evalcb)
+
+  return model
+end
+
+################################################################################
