d8e4f7bc62cd113876d0950105dfe7cf0d267a21
DanielRivasMD <danielrivasmd@gmail.com>
2021-03-13 00:09:33 +0100

    add summarize stats & heatmap plot

--- src/Utilities/stateStats.jl
+++ src/Utilities/stateStats.jl
@@ -1,6 +1,7 @@
 ################################################################################
 
 import FreqTables
+import DelimitedFiles
 
 ################################################################################
 
@@ -41,6 +42,56 @@ function stateStats(stateAr::Array{Float64, 2}, numState = 5)
     statsAr[:, ct] = tmpFq.array                                                # collect frecuency into array
   end
   return statsAr
+end
+
+################################################################################
+
+function summarizeStats(fileList::Array{String, 1}, maxChannel = 25)
+  collectAr = zeros(Float64, length(fileList), maxChannel)
+  cf = 0
+  for f in fileList
+    cf += 1
+    stAr = DelimitedFiles.readdlm(f, ',')                                       # read csv files
+    grRt = groundStateRatio(stAr)                                               # perform operations. get ground state ratio
+    collectAr[cf, 1:length(grRt)] = grRt[:, 1]                                  # collect into array
+  end
+  plotStatesHeatmap(collectAr)                                                  # plot
+  return collectAr
+end
+
+################################################################################
+
+function groundStateRatio(stateAr)
+  stateAr[1, :] ./ sum(stateAr, dims = 1)'
+end
+
+################################################################################
+
+function plotStatesHeatmap(toSummarize::Array{Float64, 2}, outimg = "groundStateRatio")
+
+  # plot layout
+  plotFig = CairoMakie.Figure()
+  heatplot = plotFig[1, 1] = CairoMakie.Axis(plotFig, title = "Ground state ratio")
+
+  # # axis labels
+  # heatplot.xlabel = "Recording Time"
+  # heatplot.ylabel = "Channels"
+  # heatplot.yticks = 1:22
+
+  # heatmap plot & color range
+  hm = CairoMakie.heatmap!(heatplot, toSummarize')
+  hm.colorrange = (0, 1)
+
+  # color bar
+  cbar = plotFig[2, 1] = CairoMakie.Colorbar(plotFig, hm, label = "Ground State proportion")
+  cbar.vertical = false
+  cbar.height = 10
+  cbar.width = CairoMakie.Relative(2 / 3)
+  cbar.ticks = 0:0.1:1
+
+  # save rendering
+  CairoMakie.save(string(outdir, "/", outimg, ".svg"), plotFig, )
+
 end
 
 ################################################################################
