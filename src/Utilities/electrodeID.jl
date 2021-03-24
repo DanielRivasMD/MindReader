e5abfd2554da212cd5520c5a2f0f88319d9e42f0
DanielRivasMD <danielrivasmd@gmail.com>
2021-03-24 11:36:41 +0100

    add support bipolar montage

--- src/Utilities/electrodeID.jl
+++ src/Utilities/electrodeID.jl
@@ -43,12 +43,40 @@ Earlobe = [
 "A2",
 ]
 
+
+Bipolar = [
+"FP1-F7",
+"F7-T7",
+"T7-P7",
+"P7-O1",
+"FP1-F3",
+"F3-C3",
+"C3-P3",
+"P3-O1",
+"FP2-F4",
+"F4-C4",
+"C4-P4",
+"P4-O2",
+"FP2-F8",
+"F8-T8",
+"T8-P8",
+"P8-O2",
+"FZ-CZ",
+"CZ-PZ",
+"P7-T7",
+"T7-FT9",
+"FT9-FT10",
+"FT10-T8",
+"T8-P8",
+]
+
 elecID = [ParasagittalL;
 ParasagittalR;
 TemporalL;
 TemporalR;
 Midline;
 Earlobe;
+Bipolar
 ]
 
 ################################################################################
