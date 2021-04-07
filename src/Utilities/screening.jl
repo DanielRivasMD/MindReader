ae44f5549d6191ba442a6fe313956059f5275a49
DanielRivasMD <danielrivasmd@gmail.com>
2021-04-07 17:06:38 +0200

    patch sensitivity & specificity function. accept multiple dispatch

--- src/Utilities/screening.jl
+++ src/Utilities/screening.jl
@@ -13,7 +13,7 @@ using OrderedCollections
 Calculate sensitivity and specificity from 2 x 2 array
 
 """
-function ss(ar::NamedArray{Int64, 2, Array{Int64, 2}, Tuple{OrderedCollections.OrderedDict{Int64, Int64}, OrderedCollections.OrderedDict{String, Int64}}})
+function ss(ar)
   return (sensitivity = ar[1, 1] / ( ar[1, 1] + ar[2, 1] ), specificity = ar[2, 2] / ( ar[2, 2] + ar[1, 2] ))
 end
 
@@ -68,9 +68,9 @@ end
 function sensspec(ssDc::Dict{String, Tuple{Array{Int64, 1}, Array{Array{Float64, 1}, 1}}}, labelVec::Array{Int64, 2})
 
   outDc = Dict{String, Array{Float64, 2}}()
-  for (k, v) in errDc
+  for (k, v) in ssDc
     tmp = zeros(1, 2)
-    (tmp[1, 1], tmp[1, 2]) = sensspec(errDc[k][1], labelVec)
+    (tmp[1, 1], tmp[1, 2]) = sensspec(ssDc[k][1], labelVec)
     outDc[k] = tmp
   end
 
