cf92ce23f1bfe201e346684853d2b530cc2cf012
DanielRivasMD <danielrivasmd@gmail.com>
2021-05-11 05:51:01 +0200

    add hidden markov model sorter

--- src/HiddenMarkovModel/hiddenMarkovModel.jl
+++ src/HiddenMarkovModel/hiddenMarkovModel.jl
@@ -16,6 +16,24 @@ end
 
 ################################################################################
 
+"sort hidden Markov model by amplitude"
+function sortHMM!(self::HMM)
+
+  ampVc = map(amplitude, self.dataM)
+  sortedAmpVc = sort(ampVc)
+  ampIxVc = map(sortedAmpVc) do y
+   findall(x -> x == y, ampVc)
+  end
+  templ = Array{Array{Float64, 1}, 1}()
+
+  for i in ampIxVc
+    push!(templ, self.dataM[i[1]])
+  end
+  self.dataM = templ
+end
+
+################################################################################
+
 function setup(v::Array{Float64, 2})
   # mPen = 5000.
   mPen = 200. #  TODO: Added code. change penalty
