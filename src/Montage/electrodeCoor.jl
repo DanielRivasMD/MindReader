b393b96f0ca2456bc7ac444e2f098a23f6415ff0
DanielRivasMD <danielrivasmd@gmail.com>
2021-05-20 14:24:56 +0200

    add electrode coordinates

---
+++ src/Montage/electrodeCoor.jl
@@ -0,0 +1,30 @@
+################################################################################
+
+electrodes = Dict()
+electrodes["Fz"] = (550, 774) # unflipped (300, 550) # (600, 350,)
+electrodes["Cz"] = (550, 584) # unflipped (490, 550) # (600, 545,)
+electrodes["Pz"] = (550, 384) # unflipped (690, 550) # (600, 740,)
+
+electrodes["A1"] = (5, 599) # unflipped (475, 5) # (55, 525,)
+electrodes["F7"] = (240, 804) # unflipped (270, 240) # (290, 330,)
+electrodes["T3"] = (170, 584) # unflipped (490, 170) # (220, 545,)
+electrodes["T5"] = (240, 354) # unflipped (720, 240) # (290, 770,)
+
+electrodes["Fp1"] = (430, 944) # unflipped (130, 430)  # (480, 185,)
+electrodes["F3"] = (390, 774) # unflipped (300, 390) # (445, 350,)
+electrodes["C3"] = (360, 584) # unflipped (490, 360) # (410, 545,)
+electrodes["P3"] = (390, 384) # unflipped (690, 390) # (445, 740,)
+electrodes["O1"] = (430, 214) # unflipped (860, 430) # (480, 910,)
+
+electrodes["A2"] = (1090, 604) # unflipped (470, 1090) # (1145, 525,)
+electrodes["F8"] = (860, 804) # unflipped (270, 860) # (910, 330,)
+electrodes["T4"] = (930, 584) # unflipped (490, 930) # (980, 545,)
+electrodes["T6"] = (860, 354) # unflipped (720, 860) # (910, 770,)
+
+electrodes["Fp2"] = (670, 944) # unflipped (130, 670)  # (720, 185,)
+electrodes["F4"] = (710, 774) # unflipped (300, 710) # (755, 350,)
+electrodes["C4"] = (740, 584) # unflipped (490, 740) # (790, 545,)
+electrodes["P4"] = (710, 384) # unflipped (690, 710) # (755, 740,)
+electrodes["O2"] = (670, 214) # unflipped (860, 670) # (720, 910,)
+
+################################################################################
