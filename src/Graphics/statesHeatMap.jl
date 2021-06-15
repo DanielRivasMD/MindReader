58964ad1eb5d1cc2352be3c7cccce6503fed8019
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-15 22:37:16 +0200

    pass args state heat & clean up

--- src/Graphics/statesHeatMap.jl
+++ src/Graphics/statesHeatMap.jl
@@ -1,7 +1,13 @@
 ################################################################################
 
 ""
-function runHeatmap(outimg::String, outsvg::String, outcsv::String, inDc::Dict{String, Tuple{Array{Int64, 1}, Array{Array{Float64, 1}, 1}}})
+function runHeatmap(shArgs, inDc::Dict{String, Tuple{Array{Int64, 1}, Array{Array{Float64, 1}, 1}}})
+
+  ts = [:svg, :csv]
+  for (s, t) ∈ zip([Symbol("out", i) for i = ts], ts)
+    tmp = shChecker(shArgs, string(s), string(t))
+    @eval $s = $tmp
+  end
 
   @info "Plotting..."
   # create array to plot
@@ -9,23 +15,23 @@ function runHeatmap(outimg::String, outsvg::String, outcsv::String, inDc::Dict{S
 
   # collect stats & write
   stats = stateStats(toHeat)
-  DelimitedFiles.writedlm( string(outcsv, outimg, "Raw.csv"), stats, ", " ) #  hardcoded csv directory
-  #  DelimitedFiles.writedlm(string(outdir, "/", outimg, ".csv"), stats, ", ")
-
-  # # add label tracks
-  # for ix in 1:size(labelAr, 2)
-  #   toHeat[ix, :] .= labelAr[1:size(toHeat, 2), ix]
-  # end
+  DelimitedFiles.writedlm( string(shArgs["outdir"], "Unlabeld", outcsv), stats, ", " )
 
   @info "Rendering..."
-  plotChannelsHeatmap(outimg, outsvg, toHeat)
+  plotChannelsHeatmap(shArgs["outdir"], outsvg, toHeat)
 
 end
 
 ################################################################################
 
 ""
-function runHeatmap(outimg::String, outsvg::String, outcsv::String, inDc::Dict{String, Tuple{Array{Int64, 1}, Array{Array{Float64, 1}, 1}}}, lbAr::Vector{Int64})
+function runHeatmap(shArgs, inDc::Dict{String, Tuple{Array{Int64, 1}, Array{Array{Float64, 1}, 1}}}, lbAr::Vector{Int64})
+
+  ts = [:svg, :csv]
+  for (s, t) ∈ zip([Symbol("out", i) for i = ts], ts)
+    tmp = shChecker(shArgs, string(s), string(t))
+    @eval $s = $tmp
+  end
 
   @info "Plotting..."
   # create array to plot
@@ -36,23 +42,17 @@ function runHeatmap(outimg::String, outsvg::String, outcsv::String, inDc::Dict{S
 
   # collect stats & write
   stats = stateStats(toHeat)
-  DelimitedFiles.writedlm( string(outcsv, outimg, "Lab.csv"), stats, ", " ) #  hardcoded csv directory
-  #  DelimitedFiles.writedlm(string(outdir, "/", outimg, ".csv"), stats, ", ")
-
-  # # add label tracks
-  # for ix in 1:size(labelAr, 2)
-  #   toHeat[ix, :] .= labelAr[1:size(toHeat, 2), ix]
-  # end
+  DelimitedFiles.writedlm( string(outdir, "Labeled", outcsv), stats, ", " )
 
   @info "Rendering..."
-  plotChannelsHeatmap(outimg, outsvg, toHeat)
+  plotChannelsHeatmap(outdir, outsvg, toHeat)
 
 end
 
 ################################################################################
 
 ""
-function plotChannelsHeatmap(outimg::String, outsvg::String, toHeat::Array{Float64, 2})
+function plotChannelsHeatmap(outdir::String, outsvg::String, toHeat::Array{Float64, 2})
 #  TODO: add channel labels by passing vector to heatmap function
   # plot layout
   plotFig = CairoMakie.Figure()
@@ -75,7 +75,10 @@ function plotChannelsHeatmap(outimg::String, outsvg::String, toHeat::Array{Float
   cbar.ticks = 1:1:5
 
   # save rendering
-  CairoMakie.save( string(outsvg, outimg, ".svg"), plotFig )
+  CairoMakie.save( string(outdir, outsvg), plotFig )
+
+end
+
 ################################################################################
 
 function shChecker(shArgs, ky, suffix)
