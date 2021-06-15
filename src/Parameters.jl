9deca26ff88a548fe9c86dab9a4ab6c19375d231
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-15 22:37:39 +0200

    add parameters

---
+++ src/Parameters.jl
@@ -0,0 +1,23 @@
+################################################################################
+
+import Parameters: @with_kw
+
+# set hyperparameters
+@with_kw mutable struct Params
+  η::Float64                   = 1e-3             # learning rate
+  epochs::Int                  = 10               # number of epochs
+  batchsize::Int               = 1000             # batch size for training
+  throttle::Int                = 5                # throttle timeout
+  device::Function             = Flux.gpu         # set as gpu, if gpu available
+  σ::Function                  = Flux.leakyrelu   # learning function
+  autoencoderHiddenLayer::Int  = 100              # hidden layer on autoencoder
+end;
+
+################################################################################
+
+import HiddenMarkovModelReaders: HMMParams
+
+# hidden Markov model parameters
+hmmParams = HMMParams(pen = 200., distance = euclDist)
+
+################################################################################
