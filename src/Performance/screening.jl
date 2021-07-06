37c674b59621c428f1ac679d771da520cc060574
DanielRivasMD <danielrivasmd@gmail.com>
2021-07-06 14:49:07 +0200

    modify screening function signatures generic

--- src/Performance/screening.jl
+++ src/Performance/screening.jl
@@ -2,12 +2,12 @@
 
 """
 
-    ss(ar)
+    sensitivitySpecifiity(ar::Matrix{T}) where T <: Number
 
 Calculate sensitivity and specificity from 2 x 2 array
 
 """
-function ss(ar::Matrix{Int64})
+function sensitivitySpecifiity(ar::Matrix{T}) where T <: Number
   if size(ar) == (2, 2)
     return (sensitivity = ar[1, 1] / ( ar[1, 1] + ar[2, 1] ), specificity = ar[2, 2] / ( ar[2, 2] + ar[1, 2] ))
   else
@@ -38,12 +38,12 @@ function sensspec(data_loader, model)
 ################################################################################
 
 "transform freqtable => dataframe"
-function convertFqDf(fq::NamedVector{Int64, Vector{Int64}, Tuple{OrderedDict{Int64, Int64}}}; colnames::Vector{String} = ["Value", "Frecuency"])
+function convertFqDf(fq::NamedVector{T, Vector{T}, Tuple{OrderedDict{T, T}}}; colnames::Vector{String} = ["Value", "Frecuency"]) where T <: Number
   return DataFrames.DataFrame([names(fq)[1] fq.array], colnames)
 end
 
 "transform freqtable => dataframe template"
-function convertFqDfTempl(fq::NamedVector{Int64, Vector{Int64}, Tuple{OrderedDict{Int64, Int64}}}; colnames::Vector{String} = ["Value", "Frecuency"], templ::Vector{Int64})
+function convertFqDfTempl(fq::NamedVector{T, Vector{T}, Tuple{OrderedDict{T, T}}}; colnames::Vector{String} = ["Value", "Frecuency"], templ::Vector{T}) where T <: Number
 
   fq = convertFqDf(fq)
 
@@ -60,12 +60,12 @@ end
 
 """
 
-    sensspec(tbVc::Array{Int64, 1}, labelVec::Vector{Int64})
+    sensitivitySpecifiity(tbVc::Array{T, 1}, labelVec::Vector{T}) where T <: Number
 
 Calculate sensitivity and specificity from a HMM
 
 """
-function sensspec(tbVc::Array{Int64, 1}, labelVec::Vector{Int64})
+function sensitivitySpecifiity(tbVc::Array{T, 1}, labelVec::Vector{T}) where T <: Number
   tbVec = copy(tbVc)
 
   # reassign frecuency labels
@@ -76,24 +76,24 @@ function sensspec(tbVc::Array{Int64, 1}, labelVec::Vector{Int64})
   positives = tbVec[labelVec[:, 1] .== 1] |> freqtable |> p -> convertFqDfTempl(p, templ = labels) |> p -> sort(p, rev = true)
   negatives = tbVec[labelVec[:, 1] .== 0] |> freqtable |> p -> convertFqDfTempl(p, templ = labels) |> p -> sort(p, rev = true)
   outNamedArray = [positives[:, 2] negatives[:, 2]]
-  return ss(outNamedArray)
+  return sensitivitySpecifiity(outNamedArray)
 end
 
 ################################################################################
 
 """
 
-    sensspec(ssDc::Dict{String, Tuple{Array{Int64, 1}, Array{Array{Float64, 1}, 1}}}, labelVec::Vector{Int64})
+    sensitivitySpecifiity(ssDc::Dict{String, Tuple{Array{T, 1}, Array{Array{Float64, 1}, 1}}}, labelVec::Vector{T}) where T <: Number
 
 Iterate on Dictionary and calculate sensitivity and specificity from a HMM
 
 """
-function sensspec(ssDc::Dict{String, Tuple{Array{Int64, 1}, Array{Array{Float64, 1}, 1}}}, labelVec::Vector{Int64})
+function sensitivitySpecifiity(ssDc::Dict{String, Tuple{Array{T, 1}, Array{Array{Float64, 1}, 1}}}, labelVec::Vector{T}) where T <: Number
 
   outDc = Dict{String, Array{Float64, 2}}()
   for (k, v) in ssDc
     outSensSpec = zeros(1, 2)
-    (outSensSpec[1, 1], outSensSpec[1, 2]) = sensspec(ssDc[k][1], labelVec)
+    (outSensSpec[1, 1], outSensSpec[1, 2]) = sensitivitySpecifiity(ssDc[k][1], labelVec)
     outDc[k] = outSensSpec
   end
 
