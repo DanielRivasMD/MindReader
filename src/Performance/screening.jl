ae8610fbaef3c1a6e85ffdf555c0c3536b1208eb
DanielRivasMD <danielrivasmd@gmail.com>
2022-03-17 13:15:45 +0100

    format & clean screening. segregate redundant code

--- src/Performance/screening.jl
+++ src/Performance/screening.jl
@@ -2,7 +2,78 @@
 
 """
 
-    sensitivitySpecificity(ar::Array{T, 2}) where T <: Number
+    sensitivitySpecificity(ssDc::Dict{S, HMM}, labelVc) where {S <: String}
+
+# Description
+Iterate on Dictionary and calculate sensitivity and specificity from a `Hidden Markov model` struct.
+
+
+See also: [`predictiveValue`](@ref)
+"""
+function sensitivitySpecificity(ssDc::Dict{S, HMM}, labelVc) where {S <: String}
+
+  outDc = Dict{S, Array{Float64, 2}}()
+  for (κ, υ) in ssDc
+    outSensSpec = zeros(1, 2)
+    (outSensSpec[1, 1], outSensSpec[1, 2]) = sensitivitySpecificity(υ.traceback, labelVc)
+    outDc[κ] = outSensSpec
+  end
+
+  return outDc
+end
+
+################################################################################
+
+"""
+
+    sensitivitySpecificity(tbVc::Array{T, 1}, labelMt::Array{T, 2}) where {T <: Number}
+
+# Description
+Calculate sensitivity and specificity from a `Hidden Markov model` struct.
+
+
+See also: [`predictiveValue`](@ref)
+"""
+function sensitivitySpecificity(tbVc::Array{T, 1}, labelMt::Array{T, 2}) where {T <: Number}
+  tbVec = copy(tbVc)
+
+  # reassign frecuency labels
+  labels = [1, 2]
+  tbVec[findall(tbVec .> 1)] .= 2
+
+  # add label columns
+  labelVc = sum(labelMt, dims = 2)
+
+  return sensitivitySpecificity(adjustFq(tbVec, labelVc, labels))
+end
+
+################################################################################
+
+"""
+
+    sensitivitySpecificity(tbVc::Array{T, 1}, labelVc::Array{T, 1}) where {T <: Number}
+
+# Description
+Calculate sensitivity and specificity from a `Hidden Markov model` struct.
+
+
+See also: [`predictiveValue`](@ref)
+"""
+function sensitivitySpecificity(tbVc::Array{T, 1}, labelVc::Array{T, 1}) where {T <: Number}
+  tbVec = copy(tbVc)
+
+  # reassign frecuency labels
+  labels = [1, 2]
+  tbVec[findall(tbVec .> 1)] .= 2
+
+  return sensitivitySpecificity(adjustFq(tbVec, labelVc, labels))
+end
+
+################################################################################
+
+"""
+
+    sensitivitySpecificity(ar::Array{T, 2}) where {T <: Number}
 
 # Description
 Calculate sensitivity and specificity from 2 x 2 array.
@@ -21,11 +92,11 @@ julia> sensitivitySpecificity(χ)
 
 See also: [`predictiveValue`](@ref)
 """
-function sensitivitySpecificity(ar::Array{T, 2}) where T <: Number
+function sensitivitySpecificity(ar::Array{T, 2}) where {T <: Number}
   if size(ar) == (2, 2)
-    return (sensitivity = ar[1, 1] / ( ar[1, 1] + ar[2, 1] ), specificity = ar[2, 2] / ( ar[2, 2] + ar[1, 2] ))
+    return (sensitivity = ar[1, 1] / (ar[1, 1] + ar[2, 1]), specificity = ar[2, 2] / (ar[2, 2] + ar[1, 2]))
   else
-    @error "Matrix does not have the proper size"
+    @error "Array does not have the proper size"
   end
 end
 
@@ -33,7 +104,7 @@ end
 
 """
 
-    predictiveValue(ar::Array{T, 2}) where T <: Number
+    predictiveValue(ar::Array{T, 2}) where {T <: Number}
 
 # Description
 Calculate positive and negative predictive values from 2 x 2 array.
@@ -52,102 +123,24 @@ julia> predictiveValue(χ)
 
 See also: [`sensitivitySpecificity`](@ref)
 """
-function predictiveValue(ar::Array{T, 2}) where T <: Number
+function predictiveValue(ar::Array{T, 2}) where {T <: Number}
   if size(ar) == (2, 2)
-    return (positive = ar[1, 1] / ( ar[1, 1] + ar[1, 2] ), negative = ar[2, 2] / ( ar[2, 2] + ar[2, 1] ))
+    return (positive = ar[1, 1] / (ar[1, 1] + ar[1, 2]), negative = ar[2, 2] / (ar[2, 2] + ar[2, 1]))
   else
-    @error "Matrix does not have the proper size"
+    @error "Array does not have the proper size"
   end
-end
-
-################################################################################
-
-"""
-
-    sensitivitySpecificity(tbVc::Array{T, 1}, labelVec::Array{T, 1}) where T <: Number
-
-# Description
-Calculate sensitivity and specificity from a `Hidden Markov model` struct.
-
-
-See also: [`predictiveValue`](@ref)
-"""
-function sensitivitySpecificity(tbVc::Array{T, 1}, labelVec::Array{T, 1}) where T <: Number
-  tbVec = copy(tbVc)
-
-  # reassign frecuency labels
-  labels = [1, 2]
-  tbVec[findall(tbVec .> 1)] .= 2
-
-  # adjust & concatenate frecuency tables
-  positives = tbVec[labelVec[:, 1] .== 1] |> freqtable |> π -> convertFqDfTempl(π, templ = labels) |> π -> sort(π, rev = true)
-  negatives = tbVec[labelVec[:, 1] .== 0] |> freqtable |> π -> convertFqDfTempl(π, templ = labels) |> π -> sort(π, rev = true)
-  outNamedArray = [positives[:, 2] negatives[:, 2]]
-  return sensitivitySpecificity(outNamedArray)
-end
-
-################################################################################
-
-"""
-
-    sensitivitySpecificity(tbVc::Array{T, 1}, labelMat::Matrix{T}) where T <: Number
-
-# Description
-Calculate sensitivity and specificity from a `Hidden Markov model` struct.
-
-
-See also: [`predictiveValue`](@ref)
-"""
-function sensitivitySpecificity(tbVc::Array{T, 1}, labelMat::Matrix{T}) where T <: Number
-  tbVec = copy(tbVc)
-
-  # reassign frecuency labels
-  labels = [1, 2]
-  tbVec[findall(tbVec .> 1)] .= 2
-
-  # add label columns
-  labelVec = sum(labelMat, dims = 2)
-
-  # adjust & concatenate frecuency tables
-  positives = tbVec[labelVec[:, 1] .== 1] |> freqtable |> π -> convertFqDfTempl(π, templ = labels) |> π -> sort(π, rev = true)
-  negatives = tbVec[labelVec[:, 1] .== 0] |> freqtable |> π -> convertFqDfTempl(π, templ = labels) |> π -> sort(π, rev = true)
-  outNamedArray = [positives[:, 2] negatives[:, 2]]
-  return sensitivitySpecificity(outNamedArray)
-end
-
-################################################################################
-
-"""
-
-    sensitivitySpecificity(ssDc::Dict{S, HMM}, labelVec) where T <: Number where S <: String
-
-# Description
-Iterate on Dictionary and calculate sensitivity and specificity from a `Hidden Markov model` struct.
-
-
-See also: [`predictiveValue`](@ref)
-"""
-function sensitivitySpecificity(ssDc::Dict{S, HMM}, labelVec) where T <: Number where S <: String
-
-  outDc = Dict{S, Array{Float64, 2}}()
-  for (κ, υ) in ssDc
-    outSensSpec = zeros(1, 2)
-    (outSensSpec[1, 1], outSensSpec[1, 2]) = sensitivitySpecificity(υ.traceback, labelVec)
-    outDc[κ] = outSensSpec
-  end
-
-  return outDc
 end
 
 ################################################################################
 
 "transform freqtable => dataframe"
-function convertFqDf(fq::NamedVector{T, Array{T, 1}, Tuple{OrderedDict{T, T}}}; colnames::Array{S, 1} = ["Value", "Frecuency"]) where T <: Number where S <: String
+function convertFqDf(fq::NamedVector{T, Array{T, 1}, Tuple{OrderedDict{T, T}}}; colnames::Array{S, 1} = ["Value", "Frecuency"]) where {T <: Number} where {S <: String}
   return DataFrames.DataFrame([names(fq)[1] fq.array], colnames)
 end
 
+
 "transform freqtable => dataframe template"
-function convertFqDfTempl(fq::NamedVector{T, Array{T, 1}, Tuple{OrderedDict{T, T}}}; colnames::Array{S, 1} = ["Value", "Frecuency"], templ::Array{T, 1}) where T <: Number where S <: String
+function convertFqDf(fq::NamedVector{T, Array{T, 1}, Tuple{OrderedDict{T, T}}}, templ::Array{T, 1}; colnames::Array{S, 1} = ["Value", "Frecuency"]) where {T <: Number} where {S <: String}
 
   fq = convertFqDf(fq)
 
@@ -169,24 +162,37 @@ function stFreqTb(fTb::NamedArray{Int64, 1})
   if sTb == 0
     fTb = [1, 2] |> freqtable |> reverse
     fTb .= 0
+
   # concatenate missing
   elseif sTb == 1
     added = copy(fTb)
     nPos = names(fTb)
+
     if sum.(nPos)[1] == 0
       NamedArrays.setnames!(added, [2], 1)
       added[1, :] .= 0
       fTb = [fTb; added]
+
     elseif sum.(nPos)[1] == 1
       NamedArrays.setnames!(added, [1], 1)
       added[1, :] .= 0
       fTb = [added; fTb]
     end
+
   # throw warning
   elseif sTb > 2
     @warn "frecuency table contains more than 2 values"
   end
   return fTb
+end
+
+################################################################################
+
+"adjust & concatenate frecuency tables"
+function adjustFq(tbVec, labelVc, labels)
+  positives = tbVec[labelVc[:, 1].==1] |> freqtable |> π -> convertFqDf(π, labels) |> π -> sort(π, rev = true)
+  negatives = tbVec[labelVc[:, 1].==0] |> freqtable |> π -> convertFqDf(π, labels) |> π -> sort(π, rev = true)
+  return [positives[:, 2] negatives[:, 2]]
 end
 
 ################################################################################
