e36bc02aad2e596d69b836f84e2657c642734ded
DanielRivasMD <danielrivasmd@gmail.com>
2022-07-06 07:02:51 +0200

    update reconstruction hidden markov model function signature

--- src/Utilities/readCSV.jl
+++ src/Utilities/readCSV.jl
@@ -10,9 +10,10 @@ Reconstruct hidden markov model.
 
 See also: [`writeHMM`](@ref)
 """
-function reconstructHMM(hmmDc::Dict{S, HMM}, filename::S, channel::S) where S <: String
-  for (κ, _) ∈ hmmDc
-    hmmDc[κ] = reconstructHMM(string(filename, channel))
+function reconstructHMM(path::S, filename::S, channels::Vector{S}) where S <: String
+  hmmDc = Dict{S, HMM}()
+  for κ ∈ channels
+    hmmDc[κ] = reconstructHMM(path, string(filename, "_", κ))
   end
   return hmmDc
 end
@@ -20,18 +21,18 @@ end
 ####################################################################################################
 
 "reconstruct hidden Markov model with empty data"
-function reconstructHMM(filename::S) where S <: String
-  return HMM([zeros(0)], readHMMmodel(filename), readHMMtraceback(filename))
+function reconstructHMM(path::S, filename::S) where S <: String
+  return HMM([zeros(0)], readHMMmodel(path, filename), readHMMtraceback(path, filename))
 end
 
 "load hidden Markov model model"
-function readHMMmodel(filename::S) where S <: String
-  return readdf(string(filename, "_model.csv"), ',') |> π -> map(χ -> π[:, χ], 1:size(π, 2))
+function readHMMmodel(path::S, filename::S) where S <: String
+  return readdf(string(path, filename, "_", "model", ".csv"), ',') |> π -> map(χ -> π[:, χ], 1:size(π, 2))
 end
 
 "load hidden Markov model traceback"
-function readHMMtraceback(filename::S) where S <: String
-  return readdf(string(filename, "_traceback.csv"), ',') |> π -> π[:, 1]
+function readHMMtraceback(path::S, filename::S) where S <: String
+  return readdf(string(path, filename, "_traceback", ".csv"), ',') |> π -> π[:, 1]
 end
 
 ####################################################################################################
