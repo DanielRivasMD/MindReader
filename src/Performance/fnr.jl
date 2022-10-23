3e48e6f2f32d3079b71f33958ed78e12118f39aa
DanielRivasMD <danielrivasmd@gmail.com>
2022-10-23 15:16:10 +0200

    add false negative rate

---
+++ src/Performance/fnr.jl
@@ -0,0 +1,31 @@
+####################################################################################################
+
+"""
+
+    FNR(ɒ::M)
+      where M <: Matrix{N}
+      where N <: Number
+
+# Description
+Calculate False Negative Rate (FNR) from contingency table or confusion matrix.
+Also called Miss Rate.
+
+# Examples
+```jldoctest
+julia> χ = [20 180; 10 1820]
+julia> FNR(χ)
+0.3333333333333333
+```
+
+
+See also: [`predictiveValue`](@ref)
+"""
+function FNR(ɒ::M) where M <: Matrix{N} where N <: Number
+  if size(ɒ) == (2, 2)
+    return ɒ[2, 1] / (ɒ[1, 1] + ɒ[2, 1])
+  else
+    @error "Array does not have the proper size"
+  end
+end
+
+####################################################################################################
