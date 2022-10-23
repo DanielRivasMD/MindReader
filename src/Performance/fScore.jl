17849a71f1b3f8d541003c0b499077b86fdfe6cc
DanielRivasMD <danielrivasmd@gmail.com>
2022-10-23 15:15:00 +0200

    add f1-score

---
+++ src/Performance/fScore.jl
@@ -0,0 +1,30 @@
+####################################################################################################
+
+"""
+
+    fScore(ɒ::M)
+      where M <: Matrix{N}
+      where N <: Number
+
+# Description
+Calculate f1-score from contingency table or confusion matrix.
+
+# Examples
+```jldoctest
+julia> χ = [20 180; 10 1820]
+julia> fScore(χ)
+0.1739130434782609
+```
+
+
+See also: [`predictiveValue`](@ref)
+"""
+function fScore(ɒ::M) where M <: Matrix{N} where N <: Number
+  if size(ɒ) == (2, 2)
+    return 2 * ((positivePV(ɒ) * sensitivity(ɒ)) / (positivePV(ɒ) + sensitivity(ɒ)))
+  else
+    @error "Array does not have the proper size"
+  end
+end
+
+####################################################################################################
