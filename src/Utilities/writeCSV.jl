0d5f500b3777e3e884c4f8e95d9a297c1cedc467
DanielRivasMD <danielrivasmd@gmail.com>
2022-10-23 21:04:39 +0200

    update write performance

--- src/Utilities/writeCSV.jl
+++ src/Utilities/writeCSV.jl
@@ -2,7 +2,32 @@
 
 """
 
-    writePerformance(performanceTp, electrode::S)
+    writePerformance(performanceDSF)
+      where DSF <: Dict{S, AF}
+      where S <: String
+      where AF <: AbstractFloat
+
+# Description
+Transform model predictive performance to table for writing.
+
+
+See also: [`getSignals`](@ref)
+"""
+function writePerformance(performanceDSF::DSF) where DSF <: Dict{S, AF} where S <: String where AF <: AbstractFloat
+  Ω = Matrix{Any}(undef, 2, length(performanceDSF))
+  for (ι, (κ, υ)) ∈ enumerate(performanceDSF)
+    Ω[:, ι] .= [string(κ), υ]
+  end
+  return Ω
+end
+
+####################################################################################################
+
+"""
+
+    writePerformance(performanceDSF::DSF, electrode::S)
+      where DSF <: Dict{S, AF}
+      where AF <: AbstractFloat
       where S <: String
 
 # Description
@@ -11,10 +36,10 @@ Transform model predictive performance to table for writing.
 
 See also: [`getSignals`](@ref)
 """
-function writePerformance(performanceTp, electrode::S) where S <: String
-  Ω = Matrix{Any}(undef, 1, length(performanceTp) + 1)
+function writePerformance(performanceDSF::DSF, electrode::S) where DSF <: Dict{S, AF} where AF <: AbstractFloat where S <: String
+  Ω = Matrix{Any}(undef, 1, length(performanceDSF) + 1)
   Ω[1, 1] = electrode
-  for (ι, υ) ∈ enumerate(performanceTp)
+  for (ι, (_, υ)) ∈ enumerate(performanceDSF)
     Ω[1, ι + 1] = υ
   end
   return Ω
@@ -24,7 +49,35 @@ end
 
 """
 
-    writePerformance(performanceDc)
+    writePerformance(filename::S, performanceDSF::DSF;
+    delim::S = ",")
+      where DSF <: Dict{S, AF}
+      where AF <: AbstractFloat
+      where S <: String
+
+# Description
+Write model predictive performance to CSV file.
+
+
+See also: [`getSignals`](@ref)
+"""
+function writePerformance(filename::S, performanceDSF::DSF; delim::S = ",") where DSF <: Dict{S, AF} where AF <: AbstractFloat where S <: String
+  writedlm(
+    filename,
+    writePerformance(performanceDSF),
+    delim,
+  )
+end
+
+####################################################################################################
+
+"""
+
+    writePerformance(performanceDc::DDS)
+      where DDS <: Dict{S, DSF}
+      where DSF <: Dict{S, AF}
+      where AF <: AbstractFloat
+      where S <: String
 
 # Description
 Transform model predictive performance to table for writing.
@@ -32,11 +85,11 @@ Transform model predictive performance to table for writing.
 
 See also: [`getSignals`](@ref)
 """
-function writePerformance(performanceDc)
-  firstPerformance = performanceDc[performanceDc |> keys .|> string |> π -> getindex(π, 1)]
-  Ω = Matrix{Any}(undef, length(performanceDc) + 1, length(firstPerformance) + 1)
+function writePerformance(performanceDDS::DDS) where DDS <: Dict{S, DSF} where DSF <: Dict{S, AF} where AF <: AbstractFloat where S <: String
+  firstPerformance = performanceDDS[performanceDDS |> keys .|> string |> π -> getindex(π, 1)]
+  Ω = Matrix{Any}(undef, length(performanceDDS) + 1, length(firstPerformance) + 1)
   Ω[1, :] .= ["Electrode"; [ι for ι ∈ string.(keys(firstPerformance))]]
-  for (ι, (κ, υ)) ∈ enumerate(performanceDc)
+  for (ι, (κ, υ)) ∈ enumerate(performanceDDS)
     Ω[ι + 1, :] = writePerformance(υ, κ)
   end
   return Ω
@@ -46,9 +99,12 @@ end
 
 """
 
-    writePerformance(filename::S, performanceTp;
+    writePerformance(filename::S, performanceDDS::DDS;
     delim::S = ",")
+      where DDS <: Dict{S, DSF}
+      where DSF <: Dict{S, AF}
       where S <: String
+      where AF <: AbstractFloat
 
 # Description
 Write model predictive performance to CSV file.
@@ -56,32 +112,10 @@ Write model predictive performance to CSV file.
 
 See also: [`getSignals`](@ref)
 """
-function writePerformance(filename::S, performanceTp, electrode::S; delim::S = ",") where S <: String
+function writePerformance(filename::S, performanceDDS::DDS; delim::S = ",") where DDS <: Dict{S, DSF} where DSF <: Dict{S, AF} where S <: String where AF <: AbstractFloat
   writedlm(
     filename,
-    writePerformance(performanceTp, electrode),
-    delim,
-  )
-end
-
-####################################################################################################
-
-"""
-
-    writePerformance(filename::S, performanceDc;
-    delim::S = ",")
-      where S <: String
-
-# Description
-Write model predictive performance to CSV file.
-
-
-See also: [`getSignals`](@ref)
-"""
-function writePerformance(filename::S, performanceDc; delim::S = ",") where S <: String
-  writedlm(
-    filename,
-    writePerformance(performanceDc),
+    writePerformance(performanceDDS),
     delim,
   )
 end
