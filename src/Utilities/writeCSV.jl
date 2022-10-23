8c0db4ddca92a4fea0a64737e4875f20a014d54e
DanielRivasMD <danielrivasmd@gmail.com>
2022-10-23 18:36:02 +0200

    update write performance

--- src/Utilities/writeCSV.jl
+++ src/Utilities/writeCSV.jl
@@ -12,10 +12,10 @@ Transform model predictive performance to table for writing.
 See also: [`getSignals`](@ref)
 """
 function writePerformance(performanceTp, electrode::S) where S <: String
-  Ω = Vector{Any}(undef, length(performanceTp) + 1)
-  Ω[1] = electrode
+  Ω = Matrix{Any}(undef, 1, length(performanceTp) + 1)
+  Ω[1, 1] = electrode
   for (ι, υ) ∈ enumerate(performanceTp)
-    Ω[ι + 1] = υ
+    Ω[1, ι + 1] = υ
   end
   return Ω
 end
@@ -40,6 +40,28 @@ function writePerformance(performanceDc)
     Ω[ι + 1, :] = writePerformance(υ, κ)
   end
   return Ω
+end
+
+####################################################################################################
+
+"""
+
+    writePerformance(filename::S, performanceTp;
+    delim::S = ",")
+      where S <: String
+
+# Description
+Write model predictive performance to CSV file.
+
+
+See also: [`getSignals`](@ref)
+"""
+function writePerformance(filename::S, performanceTp, electrode::S; delim::S = ",") where S <: String
+  writedlm(
+    filename,
+    writePerformance(performanceTp, electrode),
+    delim,
+  )
 end
 
 ####################################################################################################
