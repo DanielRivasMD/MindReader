5140b2c152c0945e1ddbda9a0f3602d35e4fc5e5
DanielRivasMD <danielrivasmd@gmail.com>
2022-10-23 15:15:50 +0200

    add negative predictive value

---
+++ src/Performance/npv.jl
@@ -0,0 +1,30 @@
+####################################################################################################
+
+"""
+
+    NPV(ɒ::M)
+      where M <: Matrix{N}
+      where N <: Number
+
+# Description
+Calculate Negative Predictive Value (NPV) from contingency table or confusion matrix.
+
+# Examples
+```jldoctest
+julia> χ = [20 180; 10 1820]
+julia> NPV(χ)
+0.994535519125683
+```
+
+
+See also: [`predictiveValue`](@ref)
+"""
+function NPV(ɒ::M) where M <: Matrix{N} where N <: Number
+  if size(ɒ) == (2, 2)
+    return ɒ[2, 2] / (ɒ[2, 2] + ɒ[2, 1])
+  else
+    @error "Array does not have the proper size"
+  end
+end
+
+####################################################################################################
