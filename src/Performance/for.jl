b6f80b912417a9a5d7d467442dfe13fb0a68ce24
DanielRivasMD <danielrivasmd@gmail.com>
2022-10-23 15:16:26 +0200

    add false omission rate

---
+++ src/Performance/for.jl
@@ -0,0 +1,30 @@
+####################################################################################################
+
+"""
+
+    FOR(ɒ::M)
+      where M <: Matrix{N}
+      where N <: Number
+
+# Description
+Calculate False Omission Rate (FOR) from contingency table or confusion matrix.
+
+# Examples
+```jldoctest
+julia> χ = [20 180; 10 1820]
+julia> FOR(χ)
+0.00546448087431694
+```
+
+
+See also: [`predictiveValue`](@ref)
+"""
+function FOR(ɒ::M) where M <: Matrix{N} where N <: Number
+  if size(ɒ) == (2, 2)
+    return ɒ[2, 1] / (ɒ[2, 1] + ɒ[2, 2])
+  else
+    @error "Array does not have the proper size"
+  end
+end
+
+####################################################################################################
